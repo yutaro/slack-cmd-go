c8a92f0c767a5eb4b9919b292696dbdc8965f142
yutaro <llgoodrr@gmail.com>
2017-10-23 18:20:02 +0900

    option and args

--- example/showOptions.go
+++ example/showOptions.go
@@ -12,11 +12,12 @@ func main() {
 
 	bot.OneCmd("test", "test options and flags",
 		func(c *scmd.Context) {
+			args := c.GetArgs()
 			flags := c.GetFlags()
 			options := c.GetOptions()
 
-			fmt.Println(flags)
-			fmt.Println(options)
+			fmt.Println("--- test ---")
+			fmt.Printf("options : %v , flags : %v , args : %v \n", options, flags, args)
 		})
 
 	bot.Start()
