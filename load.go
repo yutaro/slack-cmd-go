8bd650fba43348d639a1479a1f87a64daab7687d
yutaro <llgoodrr@gmail.com>
2017-10-25 02:21:17 +0900

    better loader

--- load.go
+++ load.go
@@ -1,8 +1,8 @@
 package scmd
 
 import (
-	"os"
-	"path/filepath"
+	"path"
+	"runtime"
 
 	"github.com/BurntSushi/toml"
 )
@@ -11,22 +11,15 @@ type Config struct {
 	TOKEN string
 }
 
-func LoadToml(path string) *Config {
+func LoadToml(confname string) *Config {
 	var conf Config
-	_, err := toml.DecodeFile(path, &conf)
-	if err == nil {
-		return &conf
-	}
+	_, cur, _, _ := runtime.Caller(1)
+	confpath := path.Join(path.Dir(cur), confname)
 
-	basepath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	_, err := toml.DecodeFile(confpath, &conf)
 	if err != nil {
 		panic(err)
 	}
-	fullpath := filepath.Join(basepath, path)
 
-	_, err = toml.DecodeFile(fullpath, &conf)
-	if err != nil {
-		panic(err)
-	}
 	return &conf
 }
